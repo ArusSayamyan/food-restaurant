f790afd28da295da0c4f6d053b7fc79647c6b501
Пользователь <arus.sayamyan@gmail.com>
2021-05-01 19:20:44 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from'./App.module.scss';
+import HeaderLogo  from "./starter/logo.png";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.App_header}>
+        <div className={styles.header_content}>
+          <div>
+            <img src={HeaderLogo} alt="HeaderLogo" className={styles.HeaderLogo}/>
+          </div>
+          <div className={styles.navbar}>
+            <ul className={styles.list}>
+              <li>HOME</li>
+              <li>MENU</li>
+            </ul>
+          </div>
+        </div>
       </header>
+      <main className={styles.App_content} >
+        <div className={styles.main_content}>
+          <h1><span>__</span>Italian food delivery<span>__</span></h1>
+          <h1 className={styles.title}>Restaurant Pronto</h1>
+          <div className={styles.buttons}>
+          <button className={styles.tableButton}>Table Reservations</button>
+          <button className={styles.OrderButton}>See MENU & Order</button>
+          </div>
+          
+
+          
+        </div>
+
+      </main>
     </div>
   );
 }
